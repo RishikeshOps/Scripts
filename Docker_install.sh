abeab39ecebe7e8c6f9b2f9792b654dcaf108d44
RishikeshOps <rishikeshmashidkar@gmail.com>
2023-01-16 13:41:20 +0530

    Update Docker_install.sh

--- Docker_install.sh
+++ Docker_install.sh
@@ -1,20 +1,5 @@
-sudo apt-get remove docker docker-engine docker.io containerd runc
-
+#!/bin/bash
 sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
-
-sudo mkdir -p /etc/apt/keyrings
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-get install docker.io -y
 
 sudo usermod -aG docker $USER
