bc76fccbcf4e5f1d66a34f1daf0a6dc928eae375
RishikeshOps <rishikeshmashidkar@gmail.com>
2023-01-26 21:50:44 +0530

    Update Docker_jenkins_install.sh

--- Docker_jenkins_install.sh
+++ Docker_jenkins_install.sh
@@ -13,6 +13,8 @@ sudo apt install default-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
+
+
 sudo apt update
 sudo apt install jenkins -y
 
