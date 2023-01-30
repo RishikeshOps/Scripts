9516abf680e1128ac66c78350b43f49dfc837a1c
RishikeshOps <rishikeshmashidkar@gmail.com>
2023-01-30 20:01:17 +0530

    Update Docker_jenkins_install.sh

--- Docker_jenkins_install.sh
+++ Docker_jenkins_install.sh
@@ -7,7 +7,7 @@ sudo apt  install docker.io -y
 #Jenkins Installetion 
 #for jenkins firstly you need java so 
 sudo apt update
-sudo apt install openjdk-11-jre
+sudo apt install openjdk-11-jre -y
 
 #Jenkins
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
@@ -16,7 +16,7 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 
 #for start jenkins
 sudo systemctl start jenkins
