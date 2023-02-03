c829223ece8d2fceefec6462ee70ba1f10e69725
Rushikesh Mashidkar <rishikeshmashidkar@gmail.com>
2023-02-03 11:20:27 +0530

    Create minikube.sh

---
+++ minikube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker $USER && newgrp docker
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+sudo snap install kubectl --classic
+minikube start --driver-docker
