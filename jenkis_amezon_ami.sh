c2efa716543f900bf123861645add973bb3082e0
Rushikesh Mashidkar <rishikeshmashidkar@gmail.com>
2023-02-16 21:45:37 +0530

    Create jenkis_amezon_ami.sh

---
+++ jenkis_amezon_ami.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update && sudo apt-get install docker.io -y
+sudo apt install default-jre -y
+sudo apt install default-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl status jenkins
+
+
+
