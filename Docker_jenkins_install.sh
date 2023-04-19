b97ae5eda8475158c31c1a80c8ab9a9b80c15de0
Rushikesh Mashidkar <rishikeshmashidkar@gmail.com>
2023-04-19 11:09:25 +0530

    Update Docker_jenkins_install.sh

--- Docker_jenkins_install.sh
+++ Docker_jenkins_install.sh
@@ -11,7 +11,7 @@ sudo apt update
 sudo apt install openjdk-11-jre -y
 
 #Jenkins
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
