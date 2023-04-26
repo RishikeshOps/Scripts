2cf89fbe18c8dd839297ada0275e124dc1b124d2
Rushikesh Mashidkar <rishikeshmashidkar@gmail.com>
2023-04-26 19:08:07 +0530

    Update Create_file.sh

--- Create_file.sh
+++ Create_file.sh
@@ -20,3 +20,13 @@ ls
 #    touch "$filename-$i$exe"
 #done
 
+# -- CREATE USER readonly  WITH ENCRYPTED PASSWORD 'bounce@readonly';
+# -- GRANT USAGE ON SCHEMA public to readonly;
+# -- ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly;
+
+# GRANT CONNECT ON DATABASE baas_service to readonly;
+# -- \c baas_service
+# ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly; --- this grants privileges on new tables generated in new database "foo"
+# GRANT USAGE ON SCHEMA public to readonly; 
+# GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO readonly;
+# GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
