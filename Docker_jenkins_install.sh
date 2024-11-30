c7bb99cdd5192490a7a68d52fbb4fd3659470441
Rushikesh Mashidkar <rishikeshmashidkar@gmail.com>
2024-11-30 15:32:53 +0530

    Update Docker_jenkins_install.sh

--- Docker_jenkins_install.sh
+++ Docker_jenkins_install.sh
@@ -11,13 +11,13 @@ sudo apt update
 sudo apt install fontconfig openjdk-17-jre
 
 #Jenkins
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins -y
+sudo apt-get install jenkins
 
 #for start jenkins
 sudo systemctl start jenkins
